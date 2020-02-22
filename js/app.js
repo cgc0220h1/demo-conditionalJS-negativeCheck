19bda605fabca0dd2d1ed0099856727c8b5c73fd
vanduc2514 <vanduc2514@gmail.com>
2020-02-23 02:58:45 +0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function negativeCheck() {
+    let num = prompt("Xin mời nhập số");
+    if (num <= 0) {
+        document.write("Số bạn nhập vào là số âm");
+    } else {
+        document.write("Số bạn nhập vào là số dương");
+    }
+}
